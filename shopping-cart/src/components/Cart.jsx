59be4935b4cb9626813a08f9a27fdc9171b98267
Vikhyath <vikhyathraims0901@gmail.com>
2024-09-17 12:01:27 +0530

    updated Cart.jsx  file

--- shopping-cart/src/components/Cart.jsx
+++ shopping-cart/src/components/Cart.jsx
@@ -1,7 +1,32 @@
-const Cart=() => {
-  return (
-    <div>
+import { useContext, useMemo } from "react"
+import { cartContext } from "../context/cartContext"
 
-    </div>
-  )
+const Cart=()=>{
+    const{cart}=useContext(cartContext);
+    const totalPrice=useMemo(()=>{
+            return cart.reduce((total,item)=>
+                total+item.price * item.quantity,0);
+    },[cart]);
+    return(
+        <div className="cart-container">
+            <h2>Your Cart</h2>
+            {cart.length>0 ? (
+                <>
+                {cart.map(item=>(
+                    <CartItem key={item.id} item={item}/>
+                ))}
+                <div cart-total>
+                    Total Price:${totalPrice.toFixed(2)}
+                </div>
+                </>
+            ):(
+                    <p>Your Cart Is Empty!!!</p>
+            )};
+
+
+        </div>
+
+    )
+    
 }
+export default Cart
