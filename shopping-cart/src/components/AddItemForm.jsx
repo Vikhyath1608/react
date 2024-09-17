a75e7f652da1073958378962a978cc72d86e7ee8
Vikhyath <vikhyathraims0901@gmail.com>
2024-09-17 10:47:12 +0530

    adde file

--- shopping-cart/src/components/AddItemForm.jsx
+++ shopping-cart/src/components/AddItemForm.jsx
@@ -1,23 +1,40 @@
+const{useState}=require("react")
+
 function AddItemForm(){
+    const [name,setName]=useState('');
+    const [price,setPrice]=useState('');
+    const [quantity,setQuantity]=useState('');
+    const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log({name,price,quantity});
+    }
+    return(
+        <div>
+            <h1>Add item</h1>
+            <form action="">
+                <input type="text"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
+                placeholder="ItemName"
+                />
+                <input type="number"
+                value={price}
+                onChange={(e)=>setPrice(e.target.value)}
+                placeholder="ItemPrice"
+                min="1"
+                />
+                 <input type="number"
+                value={quantity}
+                onChange={(e)=>setQuantity(e.target.value)}
+                placeholder="Item Quantity"
+                min="1"
+                />
+                <button type="submit" onClick={handleSubmit}>Add Item</button>
+            </form>
 
-const[name,setName]=userState('');
-const[price,setPrice]=userState('');
-const[quality,setQuality]=userState('');
+        </div>
 
-return(
-    <div>
-        <h1>Add item</h1>
-        <form action="">
-            <input type="text"
-            value={name}
-            onChange={(e)=>setName(e.target.value)}
-            placeholder="ItemName"
-            />
-        </form>
-
-    </div>
-
-)
+    )
 }
 
 export default AddItemForm
